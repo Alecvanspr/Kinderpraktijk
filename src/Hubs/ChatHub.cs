4bce8ace7fb5135c00a69bff9ad6ab32c17a0397
alecv <Alecvanspr@gmail.com>
2021-12-31 12:29:21 +0100

    Console berichten verwijderd

--- src/Hubs/ChatHub.cs
+++ src/Hubs/ChatHub.cs
@@ -7,9 +7,6 @@ public class ChatHub : Hub
     //Deze methode send een message
         public async Task SendMessage(string user, string message, string roomId)
         {
-            Console.WriteLine(user);
-            Console.WriteLine();
-            Console.WriteLine(roomId);
             await Clients.Group(roomId).SendAsync("ReceiveMessage", user, message);
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
@@ -17,14 +14,12 @@ public class ChatHub : Hub
         //Onderstaande methode zou ervoor moeten zorgen dat je in een group geconnect wordt
         public Task joinRoom(string roomId)
         {
-            Console.WriteLine("Hij is ingelogd in "+ roomId);
             return Groups.AddToGroupAsync(Context.ConnectionId, roomId);
         }
 
         //Onderstaande methode zou ervoor moeten zorgen dat je uit de groep bent gegooid als het niet meer relevant is 
         public Task LeaveGroupAsync(string roomId)
         {
-            Console.WriteLine("Hij is verliet in "+ roomId);
             return Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
         }
 }
