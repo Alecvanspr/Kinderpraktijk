fe73557d54c604fee67bfe14351922ff10c8187e
alecv <Alecvanspr@gmail.com>
2022-01-02 14:29:51 +0100

    TestFolderAangemaakt

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
