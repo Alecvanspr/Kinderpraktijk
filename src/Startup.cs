379d990e2c01b674ce8002932189a56c64b774da
alecv <Alecvanspr@gmail.com>
2022-01-16 16:45:29 +0100

    Database en project geconfigureerd

--- src/Startup.cs
+++ src/Startup.cs
@@ -30,8 +30,9 @@ namespace src
             services.AddControllersWithViews();
 
 //Deze moet later verwijderd worden,Doordat we gebruik maken van een andere DB provider 
-            services.AddDbContext<MijnContext>(options => options.UseSqlServer(Configuration.GetConnectionString("srcContextConnection")));
-
+            services.AddDbContext<MijnContext>(options => 
+                        //options.UseSqlServer(Configuration.GetConnectionString("srcContextConnection")));
+                        options.UseSqlite("Data Source=database.db"));
 
             //dit is nodig voor de identity
             services.AddIdentity<srcUser, IdentityRole>()
