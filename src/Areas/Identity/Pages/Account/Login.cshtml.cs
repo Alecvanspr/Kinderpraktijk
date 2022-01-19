8e06dbcddafe7369b5e21d6866395e62e65cd447
alecv <Alecvanspr@gmail.com>
2022-01-19 12:11:43 +0100

    git ignore aangepast

--- src/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -114,7 +114,7 @@ namespace src.Areas.Identity.Pages.Account
             var _GoogleReCHAPTCHA= _GoogleReCHAPTCHAService.VertifyResponse(Input.Token);
 
             //Hier wordt gekeken of het resultaat is gelukt. en of de score van de ReCAPTCHA boven de 0.5 is.
-            if(!_GoogleReCHAPTCHA.Result.success && _GoogleReCHAPTCHA.Result.score>=0.5){
+            if(!_GoogleReCHAPTCHA.Result.success && _GoogleReCHAPTCHA.Result.score<=0.5){
                     ModelState.AddModelError(string.Empty, "ReCAPTCHA gefaald, probeer opnieuw.");
             }else if (ModelState.IsValid)
             {
